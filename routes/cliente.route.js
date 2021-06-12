aed6b26f72ab658bdf55d204ad8231f9dbb38d8a
André <andre.lima@brsis.com.br>
2021-06-12 10:49:10 -0300

    add metodos http

--- routes/cliente.route.js
+++ routes/cliente.route.js
@@ -1,8 +1,24 @@
 const express = require('express');
 const router = express.Router();
 
-router.route('/').get((req, res, next) => {
+router.get('/', (req, res, next) => {
     res.status(200).send("Cliente");
+});
+
+router.get('/:id', (req, res, next) => {
+    res.status(200).send("Cliente 1");
+});
+
+router.post('/', async function (req, res, next) {
+	res.status(200).send("Cliente posts");
+});
+
+router.put('/:id', async function (req, res, next) {
+	res.status(200).send("Cliente put");
+});
+
+router.delete('/:id', async function (req, res, next) {
+	res.status(200).send("Cliente delete");
 });
 
 module.exports = router;
--- routes/produto.route.js
+++ routes/produto.route.js
@@ -1,8 +1,24 @@
 const express = require('express');
 const router = express.Router();
 
-router.route('/').get((req, res, next) => {
+router.get('/', (req, res, next) => {
     res.status(200).send("Produto");
+});
+
+router.get('/:id', (req, res, next) => {
+    res.status(200).send("Produto 1");
+});
+
+router.post('/', async function (req, res, next) {
+	res.status(200).send("Produto posts");
+});
+
+router.put('/:id', async function (req, res, next) {
+	res.status(200).send("Produto put");
+});
+
+router.delete('/:id', async function (req, res, next) {
+	res.status(200).send("Produto delete");
 });
 
 module.exports = router;
